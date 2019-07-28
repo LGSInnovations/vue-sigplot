8feb5324898efa3cb68553238c7d1d3fce37eff8
Michael Recachinas <mgr3yp@virginia.edu>
2019-07-28 09:21:57 +0100

    Add SigPlot.vue and ArrayLayer.vue

---
+++ src/components/ArrayLayer.vue
@@ -0,0 +1,62 @@
+<template>
+  <div></div>
+</template>
+<script>
+
+export default {
+  name: 'ArrayLayer',
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    options: {
+      type: Object,
+      default: () => {}
+    },
+    layerOptions: {
+      type: Object,
+      default: () => {}
+    },
+  },
+  // watch() {
+  //   data: {
+  //     handler (newData) {
+  //       window.console.log("Data changed. newData=" + newData)
+  //     }
+  //   },
+  //   options: {
+  //     handler (newOptions) {
+  //       window.console.log("Options changed. newOptions=" + newOptions)
+  //     },
+  //     // deep: true
+  //   },
+  //   layerOptions: {
+  //     handler (newLayerOptions) {
+  //       window.console.log("Options changed. newLayerOptions=" + newLayerOptions)
+  //     }
+  //   }
+  // },
+  beforeCreate() {
+    window.console.log("BEFORE CREATE")
+  },
+  created() {
+    window.console.log("HELLO WORLD - created")
+  },
+  mounted() {
+    window.console.log("HELLO WORLD")
+    window.console.log(this.$parent)
+    this.layer = this.$parent.plot.overlay_array(
+      this.data,
+      this.options,
+      this.layerOptions
+    )
+  },
+
+  beforeDestroy () {
+    if (this.layer) {
+      /* @TODO: do something... */
+    }
+  }
+}
+</script>
---
+++ src/components/SigPlot.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sigplot"></div>
+</template>
+
+<script>
+import sigplot from 'sigplot';
+
+export default {
+  name: 'SigPlot',
+  props: {
+    plotOptions: {
+      type: Object,
+      default: () => {}
+    },
+  },
+  // watch() {
+  //   options: {
+  //     handler (newValue) {
+  //       this.chart.update(copyObject(newValue, this.deepCopyOnUpdate), ...this.updateArgs)
+  //     },
+  //     deep: true
+  //   }
+  // },
+  mounted() {
+    this.plot = new sigplot.Plot(this.$el, this.plotOptions)
+  },
+
+  beforeDestroy () {
+    if (this.plot) {
+      /* @TODO: do something... */
+    }
+  }
+}
+</script>
+
+<style>
+div.sigplot {
+  height: 300px;
+  width: 300px;
+}
+</style>
